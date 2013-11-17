67792df419a3eca540eab3b0d91e2eb12ec7da93
pgivens4915 <pgivens4915@gmail.com>
2013-11-17 11:52:35 -0700

    Added a main loop, get char collects the down and up press this is bad

--- creature.cpp
+++ creature.cpp
@@ -3,6 +3,7 @@
 
 int Creature::drawCreature(WINDOW* window){
   mvaddch(x, y, blit);
+  return(0);
 }
 
 Creature::Creature(int inX, int inY, char inBlit){
@@ -10,10 +11,20 @@ Creature::Creature(int inX, int inY, char inBlit){
   y = inY;
   blit = inBlit;
   path.push(1001); 
+  path.push(1002);
+  path.push(1003);
+  path.push(2003);
+  path.push(2004);
+  path.push(2005);
 }
 
 int Creature::step(){
   int ret = 0;
+
+  // Return if the queue is empty
+  if(path.empty()){
+    return(-1);
+  }
 
   // Getting (x,y) from the queue
   ret = path.front();
@@ -22,5 +33,6 @@ int Creature::step(){
   // For example 120021 x = 120 y = 21
   x = ret / 1000;
   y = ret % 1000;
+  return(0);
 }
 
--- dungeon.cpp
+++ dungeon.cpp
@@ -1,33 +1,59 @@
 #include <ncurses.h>
 #include "map.h"
 #include "creature.h"
+#include <list>
 
+
+FILE* log;
 int main(){
   // Declarations
+  int gameStep(WINDOW* mainWindow, std::list<Creature*> creatureList, Map map);
   Map map;
   WINDOW *mainWindow;
   Creature rat(3,3,'r');
   int retval;
+  std::list<Creature*> creatureList;
 
   retval = map.initMap();
   if(retval != 0){
     return(1);
   }
 
+  log = fopen("log.txt", "w");
   mainWindow = initscr();
-  map.drawMap(mainWindow);
-  rat.drawCreature(mainWindow);
+  creatureList.push_front(&rat);
 
-  refresh();
-  // Waiting for input
-  getch();
-  rat.step();
-  map.drawMap(mainWindow);
-  rat.drawCreature(mainWindow);
-  refresh();
-  getch();
-  
+  fprintf(log, "DEBUG\n");// DEBUG
+
+  for(;;){
+    gameStep(mainWindow, creatureList, map);
+  }
+ 
   endwin();
 
   return 0;
+}
+
+// Takes a single step of the game
+int gameStep(WINDOW* mainWindow, std::list<Creature*> creatureList, Map map){
+  std::list<Creature*>::iterator i;
+  int retVal;
+
+  // Move the creatures
+  for(i = creatureList.begin(); i != creatureList.end(); ++i){
+    retVal = (*i)->step();
+    fprintf(log, "moving creature %i\n", retVal);// DEBUG
+  }
+  
+  // Draw everything
+  map.drawMap(mainWindow);
+  // Draw all the creatures
+  for(i = creatureList.begin(); i != creatureList.end(); ++i){
+    retVal = (*i)->drawCreature(mainWindow);
+    fprintf(log, "drawing creature %i\n", retVal);// DEBUG
+  }
+  refresh();
+  // Waiting for input
+  getch();
+
 }
